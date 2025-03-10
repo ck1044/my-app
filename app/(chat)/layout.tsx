36cb9d9093bdca5023fe9f357a9d33f2d30f1e32
ck1044 <kyum1044@gmail.com>
2025-03-10 16:49:46 +0900

    t1

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -12,7 +12,7 @@ export default function ChatLayout({
       <div className="md:flex h-full">
         {" "}
         {/* md:flex는 화면 크기가 768px이상일 때 뒤에 정의한 스타일 적용한다, 이상이면 데스탑으로 간주함 */}
-        {/* 사이드바 영역 */}
+        {/* 사이드바 영역 haha */}
         <div className="hidden md:block w-[300px]">
           {/* md:block은 데스크탑 시 보여짐, 세로로 나열, inline-block은 가로로 나열*/}
           <Sidebar />
