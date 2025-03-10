0e8c4e0210186fd60f9cd6f0bc2f65b5bd79e4e7
ck1044 <kyum1044@gmail.com>
2025-03-10 17:45:44 +0900

    :123

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-
+//test
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql, { schema });
 
